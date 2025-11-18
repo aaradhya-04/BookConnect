a92f3f642e71070f3f62cf4e7dbbad09c105d4b7
aaradhya-04 <aaradhyamittal77@gmail.com>
2025-11-18 12:50:20 +0530

    Remove Part-2, Part-3, Part-4 from repo; keep local backups

--- Part-2/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Wrap async functions so errors go to Express error handler
-module.exports = function asyncHandler(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-};
